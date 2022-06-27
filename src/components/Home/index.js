37db3a21b39562c53510983d0c1e3568eaa27a27
Monzikov Maxim <monzikovm@yandex.ru>
2022-06-27 19:21:43 +0300

    feat: Home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,12 @@
+import React from 'react';
+import { Link } from "react-router-dom";
 import "./index.scss";
-import React from 'react'
-import { Link } from "react-router-dom"
+
 
 const Home = () => {
+
   return (
-    <div className="container home-page">
+    <div className="container home-page"> 
       <div className="text-zone">
         <h1>Hi,
         I'm Maxim Monzikov
@@ -13,7 +15,7 @@ const Home = () => {
         <Link to="/contact" className="contact-button">CONTACT ME</Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
